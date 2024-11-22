9ca356cab8d6a9572c577a55dae04183dc1b960b
MYQUYEN1 <1150070037@sv.hcmunre.edu.vn>
2024-11-23 00:16:52 +0700

    QLVT

---
+++ QLVT/ApplicationDbContext/MasterDbContext/MasterDbContext.cs
@@ -0,0 +1,25 @@
+﻿
+using QLVT.Models;
+using System.Data.Entity;
+
+namespace QLVT.ApplicationDbContext
+{
+    public class MasterDbCotext:DbContext
+    {
+        public MasterDbCotext(string connectionString) : base(connectionString) { }
+        public DbSet<Login> accounts { get; set; }
+        //public DbSet<ChiNhanh> branchs { get; set; }
+        //public  DbSet<Nhom> groups { get; set; }
+        //public DbSet<VatTu> products { get; set; }
+        public DbSet<NhanVien> employees { get;set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Login>()
+                .HasKey(u => u.MaLogin);
+
+            modelBuilder.Entity<NhanVien>()
+                .HasKey(u => u.MaNV);
+        }
+    }
+}
---
+++ QLVT/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using QLVT.ApplicationDbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLVT.Controllers
+{
+    public class LoginController : Controller
+    {
+        private MasterDbCotext masterDb = new MasterDbCotext("MASTER");
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(FormCollection form)
+        {
+            string username = form["username"];
+            string password = form["password"];
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                ViewBag.Error = "Username and Password are required";
+                return View();
+            }
+
+            var user = masterDb.accounts.FirstOrDefault(u => u.MaNV == decimal.Parse(username) && u.Password == password); ;
+            if (user != null)
+            {
+                //Lay thong tin chi nhanh de luu vao session, phuc vu cho muc dich query theo chi nhanh
+                var maCN=masterDb.employees.FirstOrDefault(emp => emp.MaNV == user.MaNV).MaCN;
+                // Lưu thông tin người dùng vào session
+                Session["UserId"] = user.MaNV;
+                Session["MaCN"] = maCN;
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Invalid username or password.";
+            return View();
+        }
+    }
+}
